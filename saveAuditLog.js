e1fd03ee95ecc04aef9d54a4e6e5b9b0cb7136dc
Shamaru Primera <shamaru001@gmail.com>
2020-05-25 00:19:15 -0500

    fixed error with user id

--- saveAuditLog.js
+++ saveAuditLog.js
@@ -2,27 +2,31 @@ module.exports = function (action, db, model, options, req) {
     const  { Audits } = db;
     let id = null;
     try {
-        id = req.user.dataValues.id;
+        const { id } = req.user;
     } catch (e) {
         id = null;
     }
 
-    if (action == 'delete') {
-        Audits.create({
-            userId: id,
-            actionType: action,
-            table: model.model.name,
-            prevValues: "{}",
-            newValues: JSON.stringify(model.where),
-        });
-    }
-    else if (!model.dataValues.table) {
-        Audits.create({
-            userId: id,
-            actionType: action,
-            table: model._modelOptions.name.plural,
-            prevValues: JSON.stringify(model._previousDataValues),
-            newValues: JSON.stringify(model.dataValues),
-        });
+    try {
+        if (action == 'delete') {
+            Audits.create({
+                userId: id,
+                actionType: action,
+                table: model.model.name,
+                prevValues: "{}",
+                newValues: JSON.stringify(model.where),
+            });
+        }
+        else if (!model.dataValues.table) {
+            Audits.create({
+                userId: id,
+                actionType: action,
+                table: model._modelOptions.name.plural,
+                prevValues: JSON.stringify(model._previousDataValues),
+                newValues: JSON.stringify(model.dataValues),
+            });
+        }
+    } catch (e) {
+        console.error(`Fail to register log for the table ${model.model.name}`)
     }
 }
